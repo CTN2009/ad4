24056c1ca3c7e7b025c5bf48d5e2bde3171ef1a2
CTN01\r-aoy <r-aoyama@ctn-net.co.jp>
2024-07-25 18:42:58 +0900

    feature/0725-2

--- src/App.svelte
+++ src/App.svelte
@@ -56,18 +56,14 @@
     // Ad Common
     if (!addScript_adCommon) {
       addScript_adCommon = true;
-      (function () {
-        var uqid = "3ded5G135605b816";
-        var gid = "5";
-        var a = document.createElement("script");
-        a.dataset.uqid = uqid;
-        a.dataset.gid = gid;
-        a.id = "afadfpc-3ded5G135605b816gid5-" + Date.now();
-        a.src =
-          "//ac.ads-manage.com/fpc/cookie_js.php?scriptId=" +
-          encodeURIComponent(a.id);
-        document.head.appendChild(a);
-      })();
+      (function(){
+var uqid = "3ded5G135605b816";
+var cid  = "10";
+var a=document.createElement("script");
+a.dataset.uqid=uqid;a.dataset.cid=cid;a.id="afadfpc-3ded5G135605b816cid10-"+Date.now();
+a.src="//ac.ads-manage.com/fpc/cookie_js.php?scriptId="+encodeURIComponent(a.id);
+document.head.appendChild(a);
+})();
     }
   });
 
